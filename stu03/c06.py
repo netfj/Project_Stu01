06248671cd710cfdcd7604a49f420638931181f2
netfj <netfj@sina.com>
2018-11-07 11:21:59 +0800

    2018-11-05 23:50

--- stu03/c06.py
+++ stu03/c06.py
@@ -5,10 +5,32 @@
 """
 
 import threading, time
-# 方法一： 将要执行的方法作为参数传递给Thread的构造方法
+
+print('方法一： 将要执行的方法作为参数传递给Thread的构造方法')
+def action(arg):
+    time.sleep(1)
+    print(threading.currentThread(),
+          '[1]: {}\n'.format(arg))
+
+for i in range(5):
+    t = threading.Thread(target=action,args=(i,))
+    t.start()
+print('main thread end!')
+t.join()
 
 
+print('方法二：从 Thread 继承，并重写 run()')
+class MyThread(threading.Thread):
+    def __init__(self,arg):
+        super(MyThread,self).__init__()
+        self.arg = arg
+    def run(self):
+        time.sleep(1)
+        print('[2]',threading.Thread.getName(self),self.arg)
 
-if __name__ == "__main__":
-    main()
+for i in range(5):
+    t = MyThread(i)
+    t.start()
+print('Main is over!')
+t.join()
 
