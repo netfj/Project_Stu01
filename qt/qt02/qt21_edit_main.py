5ce96b2e758a6c9c6af26c656b6ee87e6b473848
netfj <netfj@sina.com>
2018-11-09 12:00:02 +0800

    2018-11-07

---
+++ qt/qt02/qt21_edit_main.py
@@ -0,0 +1,14 @@
+#coding:utf-8
+"""
+@info: 
+@author:NetFj @software:PyCharm @file:qt21_edit_main.py @time:2018/11/9.11:55
+"""
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from qt21_edit import *
+
+class myEdit(QtW,self):
+    super(myEdit,self).__init__()
+    self.setupUi(self)
+
