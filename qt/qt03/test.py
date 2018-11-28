c57d98bee5c0bdcb85a0a13b4fb87a1dcbf203c3
netfj <netfj@sina.com>
2018-11-28 17:02:32 +0800

    2018-11-27

--- qt/qt03/test.py
+++ qt/qt03/test.py
@@ -2,21 +2,44 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.setWindowTitle('Qt Designer!')
-        #还有很多…(略)
+import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout,
+QFrame, QSplitter)
+from PyQt5.QtCore import Qt
 
-#from Qt_Designer.py import Ui_Form
-class myForm(QtWidgets.QWidget,Ui_Form):
+class Example(QWidget):
+
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        print('这是我的业务内容.')
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    mainwindow = myForm()
-    mainwindow.show()
+        hbox = QHBoxLayout(self)    #这个可以任意
+
+        topleft = QFrame(self)
+        topleft.setFrameShape(QFrame.StyledPanel)
+
+        topright = QFrame(self)
+        topright.setFrameShape(QFrame.StyledPanel)
+
+        bottom = QFrame(self)
+        bottom.setFrameShape(QFrame.StyledPanel)
+
+        splitter1 = QSplitter(Qt.Horizontal)
+        splitter1.addWidget(topleft)
+        splitter1.addWidget(topright)
+
+        splitter2 = QSplitter(Qt.Vertical)
+        splitter2.addWidget(splitter1)
+        splitter2.addWidget(bottom)
+
+        hbox.addWidget(splitter2)
+        self.setLayout(hbox)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('窗口分隔')
+        self.show()
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
     sys.exit(app.exec_())
