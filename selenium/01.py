bf3ffa6e4f235ac6c6144d813d9e6a1910e8a821
netfj <netfj@sina.com>
2019-02-25 11:24:21 +0800

    2018-11-28

--- selenium/01.py
+++ selenium/01.py
@@ -7,3 +7,27 @@ time:   2019/2/24.15:34
 """
 
 
+import time
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+
+driver = Chrome()
+driver.get("http://www.baidu.com")
+
+assert "百度一下" in driver.title , '这不是百度的'
+
+# element = driver.find_element_by_id('kw')
+# element.clear()
+# element.send_keys('天气')
+# element.send_keys(Keys.RETURN)
+
+
+driver.find_element_by_id('kw').send_keys('天气')
+x = driver.find_element_by_id('su')
+x.click()
+
+# time.sleep(5)
+# driver.close()
+# driver.quit()
+
+print('end!')
---
+++ selenium/06.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+"""
+info:   
+author: NetFj@sina.com
+file:   06.py 
+time:   2019/2/24.19:53
+"""
+
+import requests
+response = requests.get("https://www.xuexi.cn/index.html")
+print(response.status_code)
